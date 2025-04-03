e743b2a6501ce01e511d86eda0788a804a5e0531
Yuriy <40341504+SpecialAgentOne@users.noreply.github.com>
2025-04-03 20:06:11 +0300

    Add files via upload
    Added four moisture sensors

--- water_garden.ino
+++ water_garden.ino
@@ -1,4 +1,7 @@
-#define sensorPin A0
+#define sensorPin1 A0
+#define sensorPin2 A1
+#define sensorPin3 A2
+#define sensorPin4 A3
 #define RELAY_PIN 10 // In my case for Arduino Mega 2560, constant
 
 void setup() {
@@ -11,15 +14,21 @@ void setup() {
 void loop() {
    // If value >= 600, It means that sensor is DRY (0%)
    // If value <= 290, sensor is wet, fully in water (100%)
-   int value = analogRead(sensorPin);
+   int value1 = analogRead(sensorPin1);
+   int value2 = analogRead(sensorPin2);
+   int value3 = analogRead(sensorPin3);
+   int value4 = analogRead(sensorPin4);
   
   // Print the value to the serial monitor
-  Serial.println("Analog output: " + value);
+  Serial.println("Sensor #1 output: " + value1);
+  Serial.println("Sensor #2 output: " + value2);
+  Serial.println("Sensor #3 output: " + value3);
+  Serial.println("Sensor #4 output: " + value4);
   
   // Wait for 1 second before the next reading
   delay(3000); // Can adjust for the needs
 
-  if(value > 500) {
+  if(value1 > 500 || value2 > 500 || value3 > 500 || value4 > 500) {
     Serial.println("The soil is DRY!");
     Serial.println("*** PUMP ACTIVE ***");
     digitalWrite(RELAY_PIN, HIGH);
@@ -31,5 +40,5 @@ void loop() {
     delay(2000);
   }
 
-  delay(1000); // Delay timer
+  delay(3000); // Delay timer
 }
